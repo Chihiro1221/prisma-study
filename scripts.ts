501d30caf31d69f7f8503794e68bb1aec279f82b
heart <2213595911@qq.com>
2022-08-07 18:16:55 +0800

    init prisma

--- scripts.ts
+++ scripts.ts
@@ -1,37 +1,43 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client'
+import { hash, verify } from 'argon2'
 
+const prisma = new PrismaClient()
+
+/** 创建用户 */
 async function createUser() {
   try {
+    const password = 'youzhi..'
     const res = await prisma.user.create({
       data: {
-        username: '张三',
-        password: 'youzhi..',
+        username: '李四',
+        password: await hash(password),
       },
-    });
-    console.log('创建用户成功！', res);
+    })
+    console.log('创建用户成功！', res)
   } catch (error) {
-    console.log('创建用户出现错误！', error);
+    console.log('创建用户出现错误！', error)
   }
 }
 
-// createUser();
+// createUser()
 
+/** 查找所有用户 */
 async function findUser() {
   try {
     const res = await prisma.user.findMany({
       include: {
         articles: true,
       },
-    });
-    // console.log(JSON.stringify(res, null, 2));
+    })
+    console.log(JSON.stringify(res, null, 2))
   } catch (error) {
-    console.log('查询文章失败！', error);
+    console.log('查询文章失败！', error)
   }
 }
 
 // findUser();
 
+/** 创建文章 */
 async function createArticle() {
   try {
     const res = await prisma.article.create({
@@ -40,26 +46,42 @@ async function createArticle() {
         content: 'hahahahhahahahahh',
         authorId: 1,
       },
-    });
-    console.log('创建文章成功！', res);
+    })
+    console.log('创建文章成功！', res)
   } catch (error) {
-    console.log('创建文章出现错误！', error);
+    console.log('创建文章出现错误！', error)
   }
 }
 
 // createArticle();
 
+/** 查找所有文章 */
 async function findArticle() {
   try {
     const res = await prisma.article.findMany({
       include: {
         author: true,
       },
-    });
-    console.log('查询文章成功！', res);
+    })
+    console.log('查询文章成功！', res)
   } catch (error) {
-    console.log('查询文章出现错误！', error);
+    console.log('查询文章出现错误！', error)
   }
 }
 
 // findArticle();
+
+/** 对比密码 */
+async function verifyPassword() {
+  const password = 'youzhi..'
+  const user = await prisma.user.findUnique({
+    where: {
+      username: '李四',
+    },
+  })
+  if (user) {
+    console.log(await verify(user.password, password))
+  }
+}
+
+// verifyPassword()
